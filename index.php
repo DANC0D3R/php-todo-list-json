e06b04252ab979fe004ae117895ad1cd58cb0735
Daniele Minieri <danieleminieri99@gmail.com>
2023-02-23 13:00:29 +0100

    Completato Bonus (Parte 1)

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-        <!--Fontawesome-->
+        <!-- Fontawesome -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <!-- Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
@@ -32,10 +32,14 @@
             <h1>ToDo List</h1>
             <!-- Lista -->
             <div class="list-box">
-                <div v-for="item in toDoList" class="item d-flex justify-content-between">
+                <div v-for="item in toDoList" class="item d-flex justify-content-between" :class="{ done: item.done }">
                     <p>{{ item.toDo }}</p>
+                    <button class="delete m-1 btn btn-square btn-danger">
+                        <i class="fa fa-trash"></i>
+                    </button>
                 </div>
             </div>
+            <!-- Sezione inserimento nuovo elemento -->
             <div class="input-box d-flex">
                 <input class="ps-2 rounded" type="text" v-model="language" placeholder='Inserisci elemento...'>
                 <button class="btn btn-primary add-button">Inserisci</button>
